abdd585ab37b38c96ec370f08b15a7e86b407266
EveKlieber <Kliebereveline@gmail.com>
2022-09-11 12:11:04 +0200

    rendering details work

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import  { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,6 +9,8 @@ import  { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;  // component property to expose the HEROES array for binding.
+  
   hero: Hero = {
     id: 1,
     name:'Windstorm'
@@ -18,6 +21,11 @@ export class HeroesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
